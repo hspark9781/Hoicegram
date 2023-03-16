68bb214677eed67c47b0e61f2e2acf911ab1f5af
DESKTOP-I633S8U\user <93star2@gmail.com>
2023-03-16 18:40:09 +0900

    [박현석] -sns프로젝트<hoicegram> 최초 파일 추가

---
+++ src/main/java/com/hsp/hoicegram/HoicegramApplication.java
@@ -0,0 +1,13 @@
+package com.hsp.hoicegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoicegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoicegramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsp/hoicegram/HoicegramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsp.hoicegram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoicegramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
